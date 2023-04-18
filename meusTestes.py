4d443c8f2bfa1f081993c2b467fe828846d4c621
Pedro Fernandes Barreto Costa <fernandespedrobc@gmail.com>
2023-04-18 20:03:09 -0300

    Melhoras no filtro

--- meusTestes.py
+++ meusTestes.py
@@ -2,7 +2,10 @@ import cv2
 import numpy as np
 
 # Load the image
-img = cv2.imread('img\semaforo7.jpg', cv2.IMREAD_GRAYSCALE)
+imagePath = 'img/semaforo9.jpg'
+
+img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+colorImage = cv2.imread(imagePath)
 
 # Apply Fourier transform to the image
 f = np.fft.fft2(img)
@@ -35,12 +38,26 @@ for contour in contours:
     # obtém o retângulo mínimo que contém o contorno
     x, y, w, h = cv2.boundingRect(contour)
     area = cv2.contourArea(contour)
-    if area > 523 :  # define os limites de área dos semáforos
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    if area > 523 and (h/w > 1.5 or w/h > 1.5):  # define os limites de área dos semáforos
+        cv2.drawContours(colorImage, [contour], 0, (0, 0, 255), 2)
         semaphore_contours.append(contour)
 
+        # Find the circles inside the rectangle
+        mask = np.zeros_like(img)
+        cv2.drawContours(mask, [contour], 0, 255, -1)
+        circle_contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        for circle_contour in circle_contours:
+            (x, y), radius = cv2.minEnclosingCircle(circle_contour)
+            center = (int(x), int(y))
+            radius = int(radius)
+            # Check if the circle is entirely inside the rectangle
+            if x - radius > contour[0][0][0] and x + radius < contour[0][2][0] \
+                    and y - radius > contour[0][0][1] and y + radius < contour[0][2][1]:
+                cv2.circle(colorImage, center, radius, (255, 0, 0), 2)
+
 # Display the results
-cv2.imshow('Original', img)
+cv2.imshow('Original', colorImage)
 #cv2.imshow('Filtered', iimg)
 #cv2.imshow('Binary', thresh)
 #cv2.imshow('Contours', img_contours)
